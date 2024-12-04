975b2b7af19df0a2c3249213650db36c138290a2
Pujan Rimal <157233718+PujanRimal77@users.noreply.github.com>
2024-12-04 09:48:31 -0500

    Initial commit: Smart Agriculture TS Project with Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/src/tests/**/*.test.ts", "**/src/tests/**/*.spec.ts"],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   roots: ['<rootDir>/src/tests'],
+  testMatch: ["**/*.test.ts", "**/*.spec.ts"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   }
